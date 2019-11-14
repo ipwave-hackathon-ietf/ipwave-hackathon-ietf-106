af9bb6e969bd937b7bdc212516cddc5d17346871
chrisshen <shenyiwen7@gmail.com>
2019-11-14 11:47:46 +0900

    Initial

---
+++ gst-webcam-client.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# gst-launch-1.0 v4l2src device=/dev/video1 ! video/x-raw,width=640,height=480,framerate=\(fraction\)30/1 ! videoconvert ! jpegenc ! udpsink host=2001:db8:100:15a::3 port=5000
+
+# IPv4
+# gst-launch-1.0 v4l2src ! video/x-raw,width=640,height=480 ! jpegenc ! rtpjpegpay ! udpsink host=192.168.100.198 port=5200
+
+# IPv6
+gst-launch-1.0 v4l2src ! video/x-raw,width=640,height=480 ! jpegenc ! rtpjpegpay ! udpsink host=2001:db8:100:15a::3 port=5200
---
+++ udpClient-message.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python2
+import socket
+import time
+
+# IPv4
+# server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+# IPv6
+server = socket.socket(socket.AF_INET6, socket.SOCK_DGRAM)
+
+# server.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+
+server.settimeout(0.2)
+# server.bind(("", 44444))
+message = b"OCB Test Message in a DSRC channel."
+
+while True:
+	server.sendto(message, ('2001:db8:100:15a::3', 37020)) # 2001:db8:100:15a::3
+	print("message sent!")
+	time.sleep(1)
